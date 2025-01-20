aaac65e0e7c38b495a396217e1edf34f81505318
Xofei <pitonvano05@gmail.com>
2025-01-20 09:41:18 +0300

    Добавьте файлы проекта.

---
+++ LanguageSchool/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LanguageSchool
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
