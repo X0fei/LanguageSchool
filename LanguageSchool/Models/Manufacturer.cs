f3d7ff0f8fac2d61eee4f180f601240482ffb3d8
Xofei <pitonvano05@gmail.com>
2025-01-20 09:54:32 +0300

    connect database

---
+++ LanguageSchool/Models/Manufacturer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LanguageSchool.Models;
+
+public partial class Manufacturer
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public DateOnly? StartDate { get; set; }
+
+    public virtual ICollection<Product> Products { get; } = new List<Product>();
+}
---
+++ LanguageSchool/Models/Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LanguageSchool.Models;
+
+public partial class Service
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public decimal Cost { get; set; }
+
+    public int DurationInSeconds { get; set; }
+
+    public string? Description { get; set; }
+
+    public float? Discount { get; set; }
+
+    public string? MainImagePath { get; set; }
+
+    public virtual ICollection<ClientService> ClientServices { get; } = new List<ClientService>();
+
+    public virtual ICollection<ServicePhoto> ServicePhotos { get; } = new List<ServicePhoto>();
+}
