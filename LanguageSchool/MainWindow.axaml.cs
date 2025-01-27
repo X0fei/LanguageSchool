1dbac291963038cc902a154f171bcacf0c14f78b
Xofei <pitonvano05@gmail.com>
2025-01-27 12:43:32 +0300

    login window layout

--- LanguageSchool/MainWindow.axaml.cs
+++ LanguageSchool/MainWindow.axaml.cs
@@ -2,8 +2,14 @@ using Avalonia.Controls;
 
 namespace LanguageSchool
 {
+    /// <summary>
+    /// Класс окна входа.
+    /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Конструктор окна входа.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
