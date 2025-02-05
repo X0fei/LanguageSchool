8d9171ece0dc2f4e42ba5aaf190f7b607565671c
X0fei <pitonvano05@gmail.com>
2025-02-05 19:27:58 +0300

    login button is active

--- LanguageSchool/Views/MainWindow.axaml.cs
+++ LanguageSchool/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using LanguageSchool.Services;
 
 namespace LanguageSchool.Views
 {
@@ -13,6 +14,25 @@ namespace LanguageSchool.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void LogInButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var user = UserService.GetUser(LoginTextBox.Text);
+
+            if (user is { Password: var password} && password == PasswordTextBox.Text)
+            {
+                ServicesWindow servicesWindow = new();
+                servicesWindow.Show();
+                Close();
+            }
+        }
+
+        private void TextBox_TextChanged(object? sender, Avalonia.Controls.TextChangedEventArgs e)
+        {
+            LogInButton.IsEnabled = 
+                !string.IsNullOrWhiteSpace(LoginTextBox.Text) &&
+                !string.IsNullOrWhiteSpace(PasswordTextBox.Text);
         }
     }
 }
